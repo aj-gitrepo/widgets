912b30ba445274e4f044da2dff839e41c784c918
wink-aajlak <aajlak.wink@gmail.com>
2021-11-16 07:31:45 +0530

    google transalate

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -49,3 +49,36 @@ export default Convert;
 
 // second {} is for get request to send along with body
 // third {} is for post
+
+// import React from 'react';
+// import {axios} from './axios';
+// const { useState, useEffect } = React;
+
+// const URL = 'https://jsonplaceholder.typicode.com/users';
+
+// const App = () => {
+//     const [users, setUsers] = useState([]);
+    
+//     useEffect(() => {
+//         // Add code here to fetch some users with axios and the URL variable
+//         // then update the 'users' piece of state
+//         const fetchData = async () => {
+//             const {data} = await axios.get(URL);
+//             setUsers(data);
+//         };
+        
+//         fetchData();
+//     }, [users]);
+     
+//     const renderedUsers = users.map((user) => {
+//         return <li key={user.id}>{user.name}</li>;
+//     });
+    
+//     return (
+//         <ul>
+//             {renderedUsers}
+//         </ul>
+//     );
+// }
+
+// export default App;
